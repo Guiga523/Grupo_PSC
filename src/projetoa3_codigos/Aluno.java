72362d55903d0823807f69654e6f4b0a8ace375b
thalitaspinto <thalita_souza28@hotmail.com>
2025-05-16 19:48:11 -0300

    Update Aluno.java

--- src/projetoa3_codigos/Aluno.java
+++ src/projetoa3_codigos/Aluno.java
@@ -1,5 +1,14 @@
 package projetoa3_codigos;
-public class Aluno extends Pessoa {
+
+public class Aluno extends Person {
     private String Resultados;
-    
+    public Aluno (String nome, int, idade, String resultados) {
+        super (nome, idade);
+    }
+public String getResultados () {
+    return resultados;
+}
+public void setResultados(String resultados) {
+    this.resultados = resultados;
+}
 }
