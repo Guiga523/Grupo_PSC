2b0eace3567a95f6db1ad2af5c5d86ac94efa502
Guiga523 <guilhermemagalhaessousa681@gmail.com>
2025-06-02 18:22:42 -0300

    Interface

--- src/projetoa3_codigos/Aluno.java
+++
@@ -1,6 +0,0 @@
-package projetoa3_codigos.src.projetoa3_codigos;
-
-public class Aluno extends Pessoa {
-    private String Resultados;
-    
-}
--- src/projetoa3_codigos/Professor.java
+++
@@ -1,5 +0,0 @@
-package projetoa3_codigos.src.projetoa3_codigos;
-
-public class Professor extends Pessoa {
-    private String Notas_Aluno;
-}
